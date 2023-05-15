3bfc80202ac38bda9c3e0fb228d7619f32928581
bellyxs <bellarodgs@gmail.com>
2023-05-15 08:36:07 -0300

    organizando as rotas

--- ApiBancoDigital/rotas.php
+++ ApiBancoDigital/rotas.php
@@ -1,19 +1,47 @@
 <?php
 
 use ApiBancoDigital\Controller;
+use ApiBancoDigital\Controller\CorrentistaController;
+use ApiBancoDigital\Controller\ContaController;
+use ApiBancoDigital\Controller\ChavePixController;
+use ApiBancoDigital\Controller\TransacaoController;
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 switch ($url) {
 
+    /*CORRENTISTA*/
+    case '/correntista/delete':
+       CorrentistaController::deletar();
+    break;
+
     case '/correntista/save':
-       /* CorrentistaController::*/
+       CorrentistaController::salvar();
+    break;
+
+    case 'correntista/entrar':
+        /*CorrentistaController::*/
+    break;
+
+    /*CONTA*/
+    case 'conta/listar':
+         ContaController::listar();
+    break;
+
+    case 'conta/save':
+         ContaController::salvar();
+    break;
+
+    case 'conta/delete':
+         ContaController::deletar();
     break;
 
     case 'conta/extrato':
         /*ContaController::*/
     break;
-
+    
+    
+    /*TRANSACAO*/
     case 'conta/pix/enviar':
         /*TransacaoController::*/
     break;
@@ -22,9 +50,7 @@ switch ($url) {
         /*TransacaoController::*/
     break;
 
-    case 'correntista/entrar':
-        /*CorrentistaController::*/
-    break;
+   
 
     default:
             http_response_code(403);
